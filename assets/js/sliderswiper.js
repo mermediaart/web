3974565c098668b4a599b13aa41d542e676db569
mermediaart <168295456+mermediaart@users.noreply.github.com>
2024-08-13 02:37:22 -0300

    Add files via upload

--- assets/js/sliderswiper.js
+++ assets/js/sliderswiper.js
@@ -6,15 +6,15 @@ document.addEventListener('DOMContentLoaded', function () {
             const parser = new DOMParser();
             const doc = parser.parseFromString(data, 'text/html');
             const portfolioItems = doc.querySelectorAll('#portfolio .portfolio-item');
-  
+
             const swiperWrapper = document.querySelector('.swiper-wrapper');
-  
+
             portfolioItems.forEach(item => {
                 const clone = item.cloneNode(true);
                 clone.classList.add('swiper-slide'); // Clase Swiper para los slides
                 swiperWrapper.appendChild(clone);
             });
-  
+
             // Initialize Swiper
             const swiper = new Swiper('.swiper-container', {
                 slidesPerView: 3,
@@ -28,6 +28,9 @@ document.addEventListener('DOMContentLoaded', function () {
                     prevEl: '.swiper-button-prev',
                 },
             });
+
+            // Reinitialize Lightbox after adding content
+            const lightbox = GLightbox(); // Reinitialize Lightbox
         })
         .catch(error => console.error('Error loading portfolio items:', error));
 });
